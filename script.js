25e9931a9f817601e990579b951a916996d84fd0
eyeskeem <-email@example.comeyeskeem@gmail.com>
2026-05-26 03:07:34 +0900

    start

---
+++ script.js
@@ -0,0 +1,11 @@
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: "auto",
+      centeredSlides: true,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+    git remote add origin https://github.com/eyeskeem/web-core-8.4.git
